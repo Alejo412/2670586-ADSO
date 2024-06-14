f93bb07ec4a9d82f4789d8a193e214bc0159a303
Alejo412 <alejohernandezromero4412@gmail.com>
2024-06-14 00:14:46 -0500

    consumo_AutomovilAPI

--- consumo_AutomovilAPI/js/consumo.js
+++ consumo_AutomovilAPI/js/consumo.js
@@ -137,7 +137,7 @@ function ver_listaAutomovil(endpoint= "http://localhost/automovilAPI/API/Obtener
 
     function mostrarInputsInsertarMarca(){
         let inputs = `
-        <div class="container mt-5 ">
+        <div class="container-fluid mt-5 d-flex justify-content-start">
             <div class="col-lg-6">
                 <div class="row align-items-center">
                     <input type="text" id="id_marcaInput" placeholder="Id_marca">
@@ -169,48 +169,87 @@ function ver_listaAutomovil(endpoint= "http://localhost/automovilAPI/API/Obtener
     }
 
     function mostrarInputsInsertarAuto(){
-        let inputs = `
-        <div class="container-fluid mt-5 d-flex justify-content-end ">
-            <div class="col-lg-6">
-                <div class="row align-items-center">
-                    <input type="text" id="matriculaInput" placeholder="Matricula">
-                </div>
-                <div class="row mt-2  align-items-center">
-                    <input type="text" id="marcaInput" placeholder="Marca">
-                </div>
-                <div class="row mt-2  align-items-center">
-                    <input type="text" id="modeloInput" placeholder="Modelo">
-                </div>
-                <div class="row mt-2  align-items-center">
-                    <input type="text" id="colorInput" placeholder="Color">
-                </div>
-                <div class="row mt-2  align-items-center">
-                    <input type="number" id="precioInput" placeholder="Precio">
-                </div>
-           
-                <div class="row mt-5 align-items-center">
-                    <button type="submit" id="submitButton">Insertar Automovil</button>
-                </div>
-            </div> 
-        </div>
-        `;
+        
+        let listaMarcas =  [];
+        endpoint= "http://localhost/automovilAPI/API/ObtenerMarca.php"
     
-        contentPrincipal.innerHTML = inputs;
+            fetch(endpoint)
+            .then(respuesta => respuesta.json())
+            .then(data => {
+
+                for (var i = 0; i < data.length; i++) {
+                   
+                    let idMarca = data[i].id_marca;
+                    let nombreMarca = data[i].nombre;
+                    let marca = new Marca(idMarca, nombreMarca);
+                    listaMarcas[i] = marca;
+                    
+
+                }
+                let inputs = `
+                <div class="container-fluid mt-5 d-flex justify-content-end ">
+                    <div class="col-lg-6">
+                        <div class="row align-items-center">
+                            <input type="text" id="matriculaInput" placeholder="Matricula">
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Marca
+                                </button>
+                                <ul class="dropdown-menu">`;
+                                for (let i = 0; i < data.length; i++) {
+                                    let nombreTem = listaMarcas[i].nombre;
+                                    inputs += `<li><a class="dropdown-item" id="marcaInput" href="#">${nombreTem}</a></li>`;
+                                }
+                                 inputs += `
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <input type="text" id="modeloInput" placeholder="Modelo">
+                        </div> 
+                        <div class="row mt-2  align-items-center">
+                            <input type="text" id="colorInput" placeholder="Color">
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <input type="number" id="precioInput" placeholder="Precio">
+                        </div>
+                   
+                        <div class="row mt-5 align-items-center">
+                            <button type="submit" id="submitButton">Insertar Automovil</button>
+                        </div>
+                    </div> 
+                </div>
+                `;
+                
+            
+                 contentPrincipal.innerHTML = inputs;
+                 let matriculaInput =document.getElementById("matriculaInput");
+                 let marcaInput =document.getElementById("marcaInput");
+                 let modeloInput =document.getElementById("modeloInput");
+                 let colorInput =document.getElementById("colorInput");
+                 let precioInput =document.getElementById("precioInput");
+                
     
-        let id_marcaInput =document.getElementById("id_marcaInput");
-        let nombreInput =document.getElementById("nombreInput");
-    
-        submitButton.addEventListener("click", function() {
-            let matriculaInsert = matriculaInput.value;
-            let marcaInsert = marcaInput.value;
-            let modeloInsert = modeloInput.value;
-            let colorInsert = colorInput.value;
-            let precioInsert = precioInput.value;
+                submitButton.addEventListener("click", function() {
+                    let matriculaInsert = matriculaInput.value;
+                    let marcaInsert = marcaInput.value;
+                    let modeloInsert = modeloInput.value;
+                    let colorInsert = colorInput.value;
+                    let precioInsert = precioInput.value;
+                    console.log(marcaInsert);
            
         
-           let automovil = new Automovil(matriculaInsert, marcaInsert,modeloInsert, colorInsert, precioInsert );
-           insertarAutomovil(automovil);
-        });
+                let automovil = new Automovil(matriculaInsert, marcaInsert,modeloInsert, colorInsert, precioInsert );
+                insertarAutomovil(automovil);
+                });
+
+            
+            });
+    
+        
     }
 
 
@@ -237,5 +276,333 @@ function ver_listaAutomovil(endpoint= "http://localhost/automovilAPI/API/Obtener
              console.log(data);
     
         });
+        ver_listaMarca();
+    
+
     }
+
+    function insertarAutomovil(Automovil){
+
+        let datosInsert = new FormData();
+       
+        datosInsert.append("matricula", Automovil.matricula);
+        datosInsert.append("marca", Automovil.marca);
+        datosInsert.append("modelo", Automovil.modelo);
+        datosInsert.append("color", Automovil.color);
+        datosInsert.append("precio", Automovil.precio);
+
+        console.log(Automovil.marca);
+    
+        let configuration = {
+                                method:"POST",
+                                headers:{
+                                    "Accept": "application/json",
+                                },
+                                body: datosInsert,
+                            };
+        fetch("http://localhost/AutomovilAPI/API/InsertarAuto.php", configuration)
+        .then(respuesta => respuesta.json())
+        .then(data=>{
+             console.log('Respuesta JSON servidor: ');
+             console.log(data);
+    
+        });
+        ver_listaAutomovil();
+    
+
+    }
+
+    
+    function mostrarInputsEditarMarca(){
+        let inputs = `
+        <div class="container-fluid mt-5 d-flex justify-content-start">
+      
+                <div class="col-lg-6">
+                     <div class="row align-items-center">
+                        <input type="text" id="id_marcaInput" placeholder="Id_marca">
+                    </div>
+                    <div class="row mt-2  align-items-center">
+                        <input type="text" id="nombreInput" placeholder="Nombre">
+                    </div>
+           
+                    <div class="row mt-5 align-items-center">
+                        <button type="submit" id="submitButton">Editar</button>
+                    </div>
+                </div> 
+        </div>
+        `;
+    
+        contentPrincipal.innerHTML = inputs;
+    
+        let id_marcaInput =document.getElementById("id_marcaInput");
+        let nombreInput =document.getElementById("nombreInput");
+    
+        submitButton.addEventListener("click", function() {
+            let id_marcaInsert = id_marcaInput.value;
+            let nombreInsert = nombreInput.value;
+           
+        
+           let marca = new Marca(id_marcaInsert, nombreInsert);
+           editarMarca(marca);
+           ver_listaMarca();
+        });
+    }
+
+    function editarMarca(Marca){
+        let datosInsert = new FormData();
+    
+        datosInsert.append("id_marca", Marca.id_marca);
+        datosInsert.append("nombre", Marca.nombre);
+       
+        console.log(Marca.nombre);
+    
+        let configuration = {
+                                method:"POST",
+                                headers:{
+                                    "Accept": "application/json",
+                                },
+                                body: datosInsert,
+                            };
+        fetch("http://localhost/AutomovilAPI/API/ActualizarMarca.php", configuration)
+        .then(respuesta => respuesta.json())
+        .then(data=>{
+             console.log('Respuesta JSON servidor: ');
+             console.log(data);
+    
+        });
+    }
+
+    function mostrarInputsEditarAuto(){
+        
+        let listaMarcas =  [];
+        endpoint= "http://localhost/automovilAPI/API/ObtenerMarca.php"
+    
+            fetch(endpoint)
+            .then(respuesta => respuesta.json())
+            .then(data => {
+
+                for (var i = 0; i < data.length; i++) {
+                   
+                    let idMarca = data[i].id_marca;
+                    let nombreMarca = data[i].nombre;
+                    let marca = new Marca(idMarca, nombreMarca);
+                    listaMarcas[i] = marca;
+                    
+
+                }
+                let inputs = `
+                <div class="container-fluid mt-5 d-flex justify-content-end ">
+                    <div class="col-lg-6">
+                        <div class="row align-items-center">
+                            <input type="text" id="matriculaInput" placeholder="Matricula">
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <div class="dropdown">
+                                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Marca
+                                </button>
+                                <ul class="dropdown-menu">`;
+                                for (let i = 0; i < data.length; i++) {
+                                    let nombreTem = listaMarcas[i].nombre;
+                                    inputs += `<li><a class="dropdown-item" id="marcaInput" href="#">${nombreTem}</a></li>`;
+                                }
+                                 inputs += `
+                                   
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <input type="text" id="modeloInput" placeholder="Modelo">
+                        </div> 
+                        <div class="row mt-2  align-items-center">
+                            <input type="text" id="colorInput" placeholder="Color">
+                        </div>
+                        <div class="row mt-2  align-items-center">
+                            <input type="number" id="precioInput" placeholder="Precio">
+                        </div>
+                   
+                        <div class="row mt-5 align-items-center">
+                            <button type="submit" id="submitButton">Editar</button>
+                        </div>
+                    </div> 
+                </div>
+                `;
+                
+            
+                 contentPrincipal.innerHTML = inputs;
+                let matriculaInput =document.getElementById("matriculaInput");
+                let marcaInput =document.getElementById("marcaInput");
+                let modeloInput =document.getElementById("modeloInput");
+                let colorInput =document.getElementById("colorInput");
+                let precioInput =document.getElementById("precioInput");
+                
+    
+                submitButton.addEventListener("click", function() {
+                    let matriculaInsert = matriculaInput.value;
+                    let marcaInsert = marcaInput.value;
+                    let modeloInsert = modeloInput.value;
+                    let colorInsert = colorInput.value;
+                    let precioInsert = precioInput.value;
+                    console.log(marcaInsert);
+           
+        
+                let automovil = new Automovil(matriculaInsert, marcaInsert,modeloInsert, colorInsert, precioInsert );
+                editarAutomovil(automovil);
+                ver_listaAutomovil();
+                });
+
+            
+            });
+    
+        
+    }
+
+    function editarAutomovil(Automovil){
+        let datosInsert = new FormData();
+    
+        datosInsert.append("matricula", Automovil.id_marca);
+        datosInsert.append("marca", Automovil.marca);
+        datosInsert.append("modelo", Automovil.modelo);
+        datosInsert.append("color", Automovil.color);
+        datosInsert.append("precio", Automovil.precio);
+       
+        console.log(Marca.nombre);
+    
+        let configuration = {
+                                method:"POST",
+                                headers:{
+                                    "Accept": "application/json",
+                                },
+                                body: datosInsert,
+                            };
+        fetch("http://localhost/AutomovilAPI/API/ActualizarAuto.php", configuration)
+        .then(respuesta => respuesta.json())
+        .then(data=>{
+             console.log('Respuesta JSON servidor: ');
+             console.log(data);
+    
+        });
+    }
+
+    function inputsEliminarMarca(){
+        let inputs = `
+                <div class="container mt-5"
+                    <div class="row mt-5  align-items-center ">
+                        <div class="col-lg-6 ">
+                            <input type="text" id="id_marcaInput" placeholder="Id marca">
+
+                            <button type="submit" id="submitButton">Eliminar</button>
+                        </div>
+                    </div> 
+                </div>
+                `;
+        contentPrincipal.innerHTML = inputs;
+    
+        let id_marcaInput =document.getElementById("id_marcaInput");
+       
+        submitButton.addEventListener("click", function() {
+            let id_marcaInsert = id_marcaInput.value;
+        
+           eliminarMarca(id_marcaInsert);
+           ver_listaMarca();
+        });
+    }
+
+    let parametroString;
+    function eliminarMarca(parametroString){
+        let datosInsert = new FormData();
+    
+        datosInsert.append("id_marca", parametroString);
+       
+    
+        let configuration = {
+                                method:"POST",
+                                headers:{
+                                    "Accept": "application/json",
+                                },
+                                body: datosInsert,
+                            };
+        fetch("http://localhost/AutomovilAPI/API/EliminarMarca.php", configuration)
+        .then(respuesta => respuesta.json())
+        .then(data=>{
+             console.log('Respuesta JSON servidor: ');
+             console.log(data);
+    
+        });
+    }
+
+    function inputsEliminarAuto(){
+        
+        let listaMarcas =  [];
+        endpoint= "http://localhost/automovilAPI/API/ObtenerMarca.php"
+    
+            fetch(endpoint)
+            .then(respuesta => respuesta.json())
+            .then(data => {
+
+                for (var i = 0; i < data.length; i++) {
+                   
+                    let idMarca = data[i].id_marca;
+                    let nombreMarca = data[i].nombre;
+                    let marca = new Marca(idMarca, nombreMarca);
+                    listaMarcas[i] = marca;
+                    
+
+                }
+                let inputs = `
+                <div class="container-fluid mt-5 d-flex justify-content-end"
+                    <div class="row mt-5  align-items-center ">
+                        <div class="col-lg-6 ">
+                            <input type="text" id="matriculaInput" placeholder="Matricula">
+
+                            <button type="submit" id="submitButton">Eliminar</button>
+                        </div>
+                    </div> 
+                </div>
+                `;
+                
+            
+                 contentPrincipal.innerHTML = inputs;
+                let matriculaInput =document.getElementById("matriculaInput");
+
+    
+                submitButton.addEventListener("click", function() {
+                    let matriculaInsert = matriculaInput.value;
+                   
+                    console.log(matriculaInsert);
+           
+        
+                eliminarAutomovil(matriculaInsert);
+                ver_listaAutomovil();
+                });
+
+            
+            });
+        
+    }
+
+    function eliminarAutomovil(parametroString){
+        let datosInsert = new FormData();
+    
+        datosInsert.append("matricula", parametroString );
+    
+    
+        let configuration = {
+                                method:"POST",
+                                headers:{
+                                    "Accept": "application/json",
+                                },
+                                body: datosInsert,
+                            };
+        fetch("http://localhost/AutomovilAPI/API/EliminarAuto.php", configuration)
+        .then(respuesta => respuesta.json())
+        .then(data=>{
+             console.log('Respuesta JSON servidor: ');
+             console.log(data);
+    
+        });
+    }
+
+
+
     
